dc7bb7ac77704bde0a35bb0591ff0673425e4d11
Psage23 <pamelasage3@gmail.com>
2020-01-17 11:30:07 -0700

    working on CRUD in projects

--- routers/ProjectRouter.js
+++ routers/ProjectRouter.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 //use CRUD here
 router.get('/', (req, res) => {
-    res.get()
+    db.get()
     .then(projects => {
         res.status(200).json(projects)
     })
@@ -13,6 +13,43 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', valdiateProjectID, (req, res) => {
+    db.get(req.params.id).then(project => {
+        res.status(200).json(project)
+    })
+    .catch(error => {res.status(500).json({message: "error getting project with id"})
+    })
+})
+
+//middleware
+function valdiateProjectID(req, res, next){
+    db.get(req.params.id).then(project => {
+        if(project){
+            req.project = project;
+            next();
+        }
+        else {
+            res.status(404).json({message: "project id does not exist"})
+        }
+    })
+    .catch(error => {res.status(500).json({message: "error informaton not retrieved"})})
+}
+
+
+function validateProject(req, res, next) {
+    if(req.body){
+        if(!req.body.name){
+            res.status(400).json({message: "Missing name field"})
+        }
+        else if (!req.body.description) {
+        res.status(400).json({message: "Missing description field"})
+        }
+        else {
+        next();
+        }
+    }
+    else {res.status(400).json({message: "error with project data"})}
+}
 
 
 module.exports = router;
