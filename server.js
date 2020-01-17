02ada36c4a41c7fbc82959b0bd443882511a0c34
Psage23 <pamelasage3@gmail.com>
2020-01-17 09:30:48 -0700

    created server.js and modified index.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const server = express();
+
+server.use(logger);
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('<h2>Yes! Server is running </h2>')
+});
+
+function logger(req, res, next) {
+    console.log(
+        `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+        'Origin'
+        )}`
+    );
+    next();
+}
