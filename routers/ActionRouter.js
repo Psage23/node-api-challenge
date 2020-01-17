d40d78b5296f74a761091e97dfa9c073636dceab
Psage23 <pamelasage3@gmail.com>
2020-01-17 09:56:30 -0700

    action get requests work

--- routers/ActionRouter.js
+++ routers/ActionRouter.js
@@ -1,1 +1,29 @@
+const express = require('express');
+const db = require('../data/helpers/actionModel');
+const router = express.Router();
+
 //use CRUD here
+router.get('/', (req, res) => {
+    db.get()
+        .then(actions => {
+            res.status(200).json(actions);
+        })
+        .catch(error => {
+            res.status(500).json({message: "error retreiving actions."})
+        })
+})
+
+router.get('/:id', (req, res) => {
+    db.get(req.params.id).then(action => {
+        res.status(200).json(action);
+    })
+    .catch(error => {
+        res.status(500).json({message: "Error getting action id"})
+    })
+})
+
+
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const ActionRouter = require('./routers/ActionRouter');
 const server = express();
 
 server.use(logger);
 server.use(express.json());
+server.use('/api/actions', ActionRouter);
 
 server.get('/', (req, res) => {
     res.send('<h2>Yes! Server is running </h2>')
